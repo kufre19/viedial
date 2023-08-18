829f60b685fff792fa5b4ce9cd7e749d6be2c962
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-08-18 16:47:52 +0100

    fixed checking scores

--- app/Http/Controllers/RiskAssessmentController.php
+++ app/Http/Controllers/RiskAssessmentController.php
@@ -88,28 +88,30 @@ class RiskAssessmentController extends Controller
         $risk_recommendation = "";
         $recommendation_link= "";
 
+
+
         if ($risk_score <= 6) {
             $risk_implication = "You have very low risk of developing type 2 diabetes within 10 years. It is estimated that 1 in 100 will develop type 2 diabetes.";
             $risk_recommendation = "Sign up for our preventing pre-diabetes program.";
             $recommendation_link = "#";
-        } elseif ($risk_score > 6 || $risk_score <= 11) {
-            $risk_implication = "You have low risk of developing type 2 diabetes within 10 years. It is estimated that 1 in 25 will develop type 2 diabetes. 
+        } elseif ($risk_score >= 7 && $risk_score <= 11) {
+            $risk_implication = "You have low risk of developing type 2 diabetes within 10 years. It is estimated that 7 in 25 will develop type 2 diabetes. 
             ";
             $risk_recommendation = "Sign up for our preventing pre-diabetes program ";
             $recommendation_link = "#";
 
-        } elseif ($risk_score > 11 || $risk_score <= 14) {
+        } elseif ($risk_score >= 12 && $risk_score <= 14) {
             $risk_implication = "You have moderate risk of developing type 2 diabetes within 10 years. It is estimated that 1 in 6 will develop type 2 diabetes. 
             ";
             $risk_recommendation = "Sign up for Viedial’s type 2 diabetes prevention program to reduce your risk of developing type 2 diabetes. This program will teach you how to lower the chance of developing type 2 diabetes by as much as 80%.";
             $recommendation_link = "#";
 
-        }elseif ($risk_score > 14 || $risk_score <= 20) {
+        }elseif ($risk_score >= 15 && $risk_score <= 20) {
             $risk_implication = "You have a high risk of developing type 2 diabetes within 10 years. It is estimated that 1 in 3 will develop type 2 diabetes.";
             $risk_recommendation = "Recommendation: Sign up for Viedial’s type 2 diabetes prevention program to reduce your risk of developing type 2 diabetes. This program will teach you how to lower the chance of developing type 2 diabetes by as much as 80%.";
             $recommendation_link = "#";
 
-        }elseif ($risk_score > 20 ) {
+        }elseif ($risk_score >= 21 ) {
             $risk_implication = "You have a very high risk of developing type 2 diabetes within 10 years. It is estimated that 1 in 2 will develop type 2 diabetes.";
             $risk_recommendation = "Sign up for Viedial’s type 2 diabetes prevention program to reduce your risk of developing type 2 diabetes. This program will teach you how to lower the chance of developing type 2 diabetes by as much as 80%.             ";
 
