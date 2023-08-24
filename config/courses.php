8fef7e2097fb1d8c11abec2535caae3e6d3a2e8e
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-08-24 13:11:48 +0100

    changed image route for course

--- config/courses.php
+++ config/courses.php
@@ -7,7 +7,7 @@ return[
     "4"=>[
         "title"=>"Stay Strong, Happy and Beautiful with type 2 Diabetes",
         "course_id"=>"4",
-        "image"=>"https://test.viedial.com/public/storage/course_assets/course_image/course_4.png",
+        "image"=>"https://app.viedial.com/public/storage/course_assets/course_image/course_4.png",
         "obj"=>[
             "How to cope with a diagnosis of type 2 diabetes.",
             "The type of medications used to treat type 2 diabetes.",
