3bd4e2f25735e610756faa26bd5eb23fcb4a27c3
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-08-26 17:37:49 +0100

    added else statement to dislaying results

--- resources/views/dashboard/assessment_results.blade.php
+++ resources/views/dashboard/assessment_results.blade.php
@@ -9,19 +9,45 @@
                 <div class="row">
 
                     <div class="col-12">
-                        @foreach ($result_list as $result)
-                            <div class="card">
-                                <div class="card-body">
-                                    <h4 class="card-title">Assesment {{ $result['score_for'] }}</h4>
-                                    <p class="card-text">
-                                        Name: {{ Auth::user()->name }} <br>
-                                        Age: {{ $result['age'] }} <br>
-                                        Score: {{ $result['risk_score'] }}
-                                    </p>
+                        @if (count($result_list) > 0)
+                            @foreach ($result_list as $result)
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Assesment {{ $result['score_for'] }}</h4>
+                                        <p class="card-text">
+                                            Name: {{ Auth::user()->name }} <br>
+                                            Age: {{ $result['age'] }} <br>
+                                            Score: {{ $result['risk_score'] }}
+                                        </p>
 
+                                    </div>
+                                </div>
+                            @endforeach
+                        @else
+                            <div class="box pull-up">
+                                <div class="box-body bg-img"
+                                    style="background-image: url({{ asset('view_assets/images/bg-5.png') }});"
+                                    data-overlay-light="9">
+                                    <div class="d-lg-flex align-items-center justify-content-between">
+                                        <div class="d-md-flex align-items-center mb-30 mb-lg-0 w-p100">
+                                            <img src="{{ asset('view_assets/images/svg-icon/color-svg/custom-14.svg') }}"
+                                                class="img-fluid max-w-150" alt="" />
+                                            <div class="ml-30">
+                                                <h4 class="mb-10">Take Our Advance Risk Assesment Test !</h4>
+                                                <p class="mb-0 text-fade">Check if you are at risk of having a heart attack,
+                                                    stroke, kidney failure etc. </p>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <a href="{{ route('risk.assessment.start') }}"
+                                                class="waves-effect waves-light btn-block btn btn-dark"
+                                                style="white-space: nowrap;">Start Now!</a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        @endforeach
+                        @endif
+
 
                     </div>
 
