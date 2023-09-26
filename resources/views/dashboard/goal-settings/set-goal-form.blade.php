b385cd1bf5868ea8522b5dfe05ee2fb43dc69fb7
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-09-26 09:23:41 +0100

    same as last

--- resources/views/dashboard/goal-settings/set-goal-form.blade.php
+++ resources/views/dashboard/goal-settings/set-goal-form.blade.php
@@ -1,17 +1,28 @@
-@if (session()->get('bmi') > 25 )
-    <p class="my-10 font-size-16 font-weight-bold">
-        Set your weight loss goal
-    </p>
+@if (session()->get('bmi') > 25)
+    @if (session()->get('set-goal') == false)
+        <p class="my-10 font-size-16 font-weight-bold">
+            Set your weight loss goal
+        </p>
 
-    <form action="" class="form" id="form-set-goals">
-        <div class="form-group col-lg-4 col-sm-12 ">
-            <label>How much weight you want to lose per week</label>
-            <input type="text" class="form-control" placeholder="Enter between 0.1 to 1.5 KG" size="10"
-                id="weight_goal_input">
+        <form action="" class="form" id="form-set-goals">
+            <div class="form-group col-lg-4 col-sm-12 ">
+                <label>How much weight you want to lose per week</label>
+                <input type="text" class="form-control" placeholder="Enter between 0.1 to 1.5 KG" size="10"
+                    id="weight_goal_input">
 
-        </div>
+            </div>
 
-    </form>
+        </form>
+    @else
+        <p class="my-10 font-size-16 font-weight-bold">
+            Your health goal has been set
+        </p>
+
+        <p class="my-10 font-size-16 font-weight-bold">
+            Weight loss goal per week for you is: 1.5 KG
+        </p>
+
+    @endif
 @else
     <p class="my-10 font-size-16 font-weight-bold"> Awesome You're currently at moderate
         BMI let's help you maintain this</p>
