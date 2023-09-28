cd68ee128dc9c208c66fa2ed207aa8c3be7f94d8
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-09-28 04:22:39 +0100

    workign on dashboard

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -4,6 +4,24 @@
 @section('main-content')
     <div class="content-wrapper">
         <div class="container-full">
+            <div class="content-header">
+                <div class="d-flex align-items-center">
+                    <div class="mr-auto">
+                        <h3 class="page-title">Welcome, {{Auth::user()->name}}</h3>
+                        {{-- <div class="d-inline-block align-items-center">
+                            <nav>
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-home-outline"></i></a></li>
+                                    <li class="breadcrumb-item" aria-current="page">e-Commerce</li>
+                                    <li class="breadcrumb-item active" aria-current="page">Products</li>
+                                </ol>
+                            </nav>
+                        </div> --}}
+                    </div>
+
+                </div>
+            </div>
+
             <!-- Main content -->
             <section class="content">
                 <div class="row fx-element-overlay">
