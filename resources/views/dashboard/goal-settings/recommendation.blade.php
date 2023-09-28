8d54c81f6927d157dc5b526cc786f2d248717dac
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-09-28 04:12:42 +0100

    same as last

--- resources/views/dashboard/goal-settings/recommendation.blade.php
+++ resources/views/dashboard/goal-settings/recommendation.blade.php
@@ -14,7 +14,7 @@
 
                         <div class="box pull-up ">
                             <div class="box-body ">
-                                <h4 class="box-title">How Long ?
+                                <h4 class="box-title">How long ?
                                     <p class="subtitle font-size-14 mb-0">
                                         It will take you {{$goalSetData->time_for_ten_percent_weight}} weeks to lose 10% of your body weight. At this time, you
                                         may start
@@ -28,7 +28,7 @@
 
                         <div class="box pull-up ">
                             <div class="box-body ">
-                                <h4 class="box-title">What You Gain !
+                                <h4 class="box-title">What you gain !
                                     <p class="subtitle font-size-14 mb-0">
                                         It will take you {{$goalSetData->time_for_healthy_weight}} weeks to achieve a healthy body weight. At this time, you
                                         may have developed healthy habits that will help you achieve sustained
